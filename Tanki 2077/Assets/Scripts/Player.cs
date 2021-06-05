a601179274fa7af5f079c621176c953a8d065034
qweu-studio <56268258+qweu-studio@users.noreply.github.com>
2021-06-05 16:40:47 +0300

    Ебаный рот этого казино блять

--- Tanki 2077/Assets/Scripts/Player.cs
+++ Tanki 2077/Assets/Scripts/Player.cs
@@ -3,35 +3,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : MonoBehaviourPun, IPunObservable
 {
-    [SerializeField] private float _speed;
-    [SerializeField] private float _speedRotation;
-    [SerializeField] private Transform _tower;
-    
-    private PhotonView _photonView;
+    public LocalPlayer LocalPlayer;
 
-
-    private void Start()
+    private void Awake()
     {
-        _photonView = GetComponent<PhotonView>();
-    }
-
-    private void Update()
-    {
-        if (_photonView.IsMine)
+        if (!photonView.IsMine)
         {
-            float vertical = Input.GetAxis("Vertical") * _speed * Time.deltaTime;
-            transform.Translate(new Vector3(0f, vertical, 0f));
-
-            float RotationZ = Input.GetAxis("Horizontal") * _speedRotation * Time.deltaTime;
-            transform.Rotate(0f, 0f, -RotationZ);
-
-            Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-            float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-            _tower.rotation = Quaternion.Euler(0f, 0f, rotZ);
-
+            LocalPlayer.enabled = false;
+            Destroy(LocalPlayer.camera.gameObject);
         }
     }
 
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(LocalPlayer.Tower.rotation);
+        }
+        else
+        {
+            LocalPlayer.Tower.rotation = (Quaternion)stream.ReceiveNext();
+        }
+    }
 }
